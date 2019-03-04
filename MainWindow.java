1197b56ce5dbbf4bb37fb1511d7e7e81bcaab522
YUR0ii <ianstumay@gmail.com>
2019-03-04 13:10:25 -0500

    h

--- MainWindow.java
+++ MainWindow.java
@@ -1,11 +1,16 @@
 import javax.swing.JWindow;
 
-public class MainWindow extends JWindow {
-
+public class MainWindow extends JWindow
+{
+	MainWindow()
+	{
+		setSize(500,800);
+		setVisible(true);
+	}
+	
 	public static void main(String[] args)
 	{
 		new MainWindow();
 		//epic!
 	}
-
 }
