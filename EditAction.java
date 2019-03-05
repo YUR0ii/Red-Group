fe2959a40d6ba505570ec0e62e9376297122aca3
xzhang9754 <xzhang9754@CHHB2ZQDXV1.inst.hcpss.org>
2019-03-05 13:09:45 -0500

    jspinner is dumb

--- EditAction.java
+++ EditAction.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Calendar;
+
 import javax.swing.*;
 
 public class EditAction extends JPanel implements ActionListener{
@@ -15,8 +17,46 @@ public class EditAction extends JPanel implements ActionListener{
 		
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
-		panel.setSize(frameWidth, frameHeight);
+		GridBagConstraints c = new GridBagConstraints();
 		
+		JRadioButton setUrgentStatus = new JRadioButton("Urgent");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 0;
+		panel.add(setUrgentStatus, c);
+		
+		JRadioButton setCurrentStatus = new JRadioButton("Current");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 1;
+		c.gridy = 0;
+		panel.add(setCurrentStatus, c);
+		
+		JRadioButton setEventualStatus = new JRadioButton("Eventual");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 2;
+		c.gridy = 0;
+		panel.add(setEventualStatus, c);
+		
+		JRadioButton setInactiveStatus = new JRadioButton("Inactive");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 3;
+		c.gridy = 0;
+		panel.add(setInactiveStatus, c);
+		
+		JCheckBox urgentDateBox = new JCheckBox("Urgent");
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 1;
+		panel.add(urgentDateBox, c);
+		
+		SpinnerDateModel spinnerModel = new SpinnerDateModel();
+		JSpinner urgentDateSelect = new JSpinner();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 2;
+		panel.add(setInactiveStatus, c);
+		
+		frame.add(panel);
 		frame.setVisible(true);
 	}
 
