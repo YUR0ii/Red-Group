336c8b5e33459487b4da1fca2f5ee9b7bbbf9f32
mzwall6038 <mzwall6038@CHHB2ZPFXV1.inst.hcpss.org>
2019-03-05 13:08:55 -0500

    Updated 3/5

--- MainPage.java
+++ MainPage.java
@@ -7,7 +7,7 @@ public class MainPage extends JPanel implements ActionListener{
 	private JFrame mainFrame=new JFrame();
 	private JScrollPane scroll=new JScrollPane();
 	private JMenuBar menuBar=new JMenuBar();
-	private JMenuItem file=new JMenu("File"); 
+	private JMenu file=new JMenu("File"); 
 	private JPopupMenu fileMenu=new JPopupMenu();
 	private JMenuItem save=new JMenuItem("Save a backup");
 	private JMenuItem restore=new JMenuItem("Restore a backup");
@@ -29,12 +29,13 @@ public class MainPage extends JPanel implements ActionListener{
 		fileMenu.add(save);
 		fileMenu.add(restore);
 		fileMenu.add(print);
-		MouseListener fileListener = new fileListener();
-		MouseListener closedListener = new closedListener();
-		file.addMouseListener(fileListener);
-		closed.addMouseListener(closedListener);
-		quit.addMouseListener(fileListener);
-		menuBar.addMouseListener(fileListener);
+		file.addMouseListener(new menuListener());
+		closed.addMouseListener(new menuListener());
+		quit.addMouseListener(new menuListener());
+		save.addMouseListener(new fileListener());
+		restore.addMouseListener(new fileListener());
+		print.addMouseListener(new fileListener());
+		this.add(input);
 		mainFrame.setLocation(400,400);
 		mainFrame.setSize(200,200);
 		mainFrame.setVisible(true);
@@ -61,35 +62,26 @@ public class MainPage extends JPanel implements ActionListener{
 		MainPage page=new MainPage();
 	}
 	class fileListener extends MouseAdapter {
-		 public void mousePressed(MouseEvent e) {
-		       makeMenu(e);
-		    }
-
-	    public void mouseReleased(MouseEvent e) {
-	       makeMenu(e);
-	    }
-
-	    private void makeMenu(MouseEvent e) {
-	        if (e.isPopupTrigger()) {
-	            fileMenu.show(e.getComponent(),
-	                       e.getX(), e.getY());
-	        }
-	    }
+		public void mouseClicked(MouseEvent e) {
+			if(e.getComponent().equals(save)) {
+				 
+			}else if(e.getComponent().equals(restore)) {
+				
+			}else if(e.getComponent().equals(print)) {
+				
+			}
+			fileMenu.setVisible(false);
+		}
 	}
-	class closedListener extends MouseAdapter {
-		 public void mousePressed(MouseEvent e) {
-		        openClosed(e);
-		    }
-
-	    public void mouseReleased(MouseEvent e) {
-	       openClosed(e);
-	    }
-
-	    private void openClosed(MouseEvent e) {
-	        if (e.isPopupTrigger()) {
-	           
-	        }
-	    }
+	class menuListener extends MouseAdapter{
+		public void mouseClicked(MouseEvent e) {
+			if(e.getComponent().equals(quit)) {
+				 mainFrame.dispose();
+			}else if(e.getComponent().equals(closed)) {
+				
+			}else if(e.getComponent().equals(file)) {
+				fileMenu.show(e.getComponent(),e.getX(),e.getY());
+			}
+		}
 	}
-	
 }
