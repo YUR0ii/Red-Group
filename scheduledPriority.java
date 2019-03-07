f895e7953878c962838d31961e978d04b5646f60
Harun <harungopal@gmail.com>
2019-03-07 12:45:34 -0500

    scheduledPriority Class Complete

--- scheduledPriority.java
+++ scheduledPriority.java
@@ -1,4 +1,35 @@
+import java.util.*;
+
 
 public class scheduledPriority {
-
+	
+	String level;
+	Date date;
+	
+	scheduledPriority(String Level, Date Date){
+		level = Level;
+		date = Date;
+	}
+	
+	public String getLevel(){
+		return level;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setLevel(String Level) {
+		level = Level;
+	}
+	
+	public void setDate(Date Date) {
+		date = Date;
+	}
+	
+	public void setDateLevel(Date Date,String Level){
+		date = Date;
+		Level = level;
+	}
+	
 }
