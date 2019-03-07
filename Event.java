24804573e1c49637b300fc5a845fdc8cb8e4e6e0
Harun <harungopal@gmail.com>
2019-03-07 12:14:15 -0500

    Set up the Structure of the Event Classes - Harun 3/7

--- Event.java
+++ Event.java
@@ -1,4 +1,41 @@
+import java.util.*;
 
-public class Event {
 
+public abstract class Event {
+	
+	private Date date;
+	private String sentence;
+	
+	Event(){
+		createSentence();
+		//Should never be called here
+		System.out.println("You called the event superclass constructor");
+	}
+	
+	//Creates the sentence to be stores until retrieved
+	//priority and comment events use given priorities and comments
+	//respectively to create these sentences
+	public abstract String createSentence();
+	
+	public String getSentence() {
+		return sentence;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	//Returns the type of event it is
+	//Either the String "Comment" or "Priority"
+	//Corresponding to the type of event of course
+	public abstract String getType();
+	
+	
+	//Will return the new and old comment if
+	//it contains a commentEvent. If not, then returns null.
+	//please check what type of event it is using getType
+	//before calling either of these two methods
+	public abstract String getOldComment();
+	public abstract String getNewComment();
+	
 }
---
+++ commentEvent.java
@@ -0,0 +1,29 @@
+
+public class commentEvent extends Event{
+	
+	
+	@Override
+	public String createSentence() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return "Comment";
+	}
+
+	@Override
+	public String getOldComment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNewComment() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
