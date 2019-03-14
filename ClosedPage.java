3f2e55326802a4d70db24e5e451a8c1aba5123bb
mzwall6038 <mzwall6038@CHHB2ZPFXV1.inst.hcpss.org>
2019-03-14 13:10:38 -0400

    Updated 3/14

--- ClosedPage.java
+++ ClosedPage.java
@@ -18,35 +18,35 @@ import javax.swing.table.DefaultTableModel;
 public class ClosedPage extends MainPage {
 	private ArrayList<taskContainer> closedContainers=new ArrayList<taskContainer>();
 
-		ClosedPage(){
-			JFrame closeFrame=new JFrame();
-			JTextArea textArea = new JTextArea(30, 30);
-			textArea.setEditable(false);
-			JPanel test = new JPanel();
-			Task task = new Task("norman testing");
-			task.setComplete(true);
-			completeTasks.add(task);
-			textArea.append("Completed Tasks\n");
-	        for (int i = 0; i<completeTasks.size(); i++) {
-	        	textArea.append(completeTasks.get(i).getName()+"\n");
-	       }
-	        for (int i = 0; i<incompleteTasks.size(); i++) {
-	        	textArea.append(incompleteTasks.get(i).getName()+"\n");
-	       }
+	ClosedPage(){
+		JFrame closeFrame=new JFrame();
+		JTextArea textArea = new JTextArea(30, 30);
+		textArea.setEditable(false);
+		JPanel test = new JPanel();
+		Task task = new Task("norman testing");
+		task.setComplete(true);
+		completeTasks.add(task);
+		textArea.append("Completed Tasks\n");
+		for (int i = 0; i<completeTasks.size(); i++) {
+			textArea.append(completeTasks.get(i).getName()+"\n");
+		}
+		for (int i = 0; i<incompleteTasks.size(); i++) {
+			textArea.append(incompleteTasks.get(i).getName()+"\n");
+		}
 
-	    	JScrollPane scroll = new JScrollPane(textArea);
+		JScrollPane scroll = new JScrollPane(textArea);
 
-	        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-	        scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-	        scroll.setBounds(0, 0, 400, 400);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scroll.setBounds(0, 0, 400, 400);
 
-	        textArea.setBackground(new Color(247,232,210));
-	        closeFrame.setContentPane(scroll);
-	        closeFrame.setLocation(550, 250);
-	        closeFrame.pack();
-	        closeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-	        closeFrame.setVisible(true);
-	        closeFrame.setResizable(false);
+		textArea.setBackground(new Color(247,232,210));
+		closeFrame.setContentPane(scroll);
+		closeFrame.setLocation(550, 250);
+		closeFrame.pack();
+		closeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		closeFrame.setVisible(true);
+		closeFrame.setResizable(false);
 
 	}
 }
